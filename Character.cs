3cad397eb69f30dda4f1844c60169fb58d61d6c9
soorek <soorek.yellow@gmail.com>
2025-03-07 22:52:42 +0300

    Добавьте файлы проекта.

---
+++ Character.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpg
+{
+    internal abstract class Character
+    {
+        public string Name { get; protected set; }
+        public int Health { get; protected set; }
+        public int AttackPower { get; protected set; }
+        public int Level { get; protected set; }
+
+        public Character(string name,  int health, int attackPower, int level)
+        {
+            Name = name;
+            Health = health;
+            AttackPower = attackPower;
+            Level = level;
+        }
+        
+        public void TakeDamage (int damage)
+        {
+            Health -= damage;
+            if (Health <= 0)
+            {
+                Console.WriteLine($"{Name} погиб.");
+                Health = 0;
+            }
+        }
+
+        public void Attack(Character target)
+        {
+            target.TakeDamage(AttackPower);
+            Console.WriteLine($"{Name} атакует {target.Name} и наносит {AttackPower} урона");
+        }
+
+        public bool IsAlive()
+        {
+            return Health > 0 ? true : false;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpg
+{
+    internal class Player : Character
+    {
+        public int Expirience { get; private set; }
+
+        public Player(string name, int health, int attackPower, int level)
+            :base(name, health, attackPower, level)
+        {
+            Expirience = 0;
+        }
+
+        private void LevelUp(int exp)
+        {
+            Level++;
+            Console.WriteLine("Вы повысили уровень, выберите навык для улучшения " +
+                "hp / dmg");
+            if(Console.ReadLine() == "hp")
+            {
+                Health += 100;
+                Console.WriteLine("Ваше здоровье увеличено на 100!");
+            }
+            else if(Console.ReadLine() == "dmg")
+            {
+                AttackPower += 20;
+                Console.WriteLine("Урон увеличен на 20 единиц");
+            }
+        }
+
+    }
+}
